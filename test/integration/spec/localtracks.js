5da1bb29c2a225356e5a6b084e85d0e4184b84f5
Makarand Patwardhan <mpatwardhan@twilio.com>
2020-05-28 22:10:54 -0700

    mark test unstable

--- test/integration/spec/localtracks.js
+++ test/integration/spec/localtracks.js
@@ -164,20 +164,42 @@ describe('replaceTrack', function() {
     await assertMediaFlow(aliceRoom, true, `Unexpected media flow after replacing track: ${aliceRoom.sid}`);
   });
 
+  it('track can be disabled after publishing (@unstable: JSDK-2852)', async () => {
+    const bobVideoTrackA = await createLocalVideoTrack(Object.assign({ name: 'trackA' }, smallVideoConstraints));
+    await waitFor(bobLocal.publishTrack(bobVideoTrackA), `Bob to publish video trackA: ${roomSid}`);
+    bobVideoTrackA.enable(false);
+    assert.equal(bobVideoTrackA.isEnabled, false);
+
+    // wait for Alice to see Bob's track
+    const bobRemote = aliceRoom.participants.get(bobRoom.localParticipant.sid);
+    await waitFor(tracksSubscribed(bobRemote, 1), `wait for alice to subscribe to Bob's tracks: ${roomSid}`);
+
+    const bobRemoteTrack = [...bobRemote.videoTracks.values()][0];
+    assert.equal(bobRemoteTrack.track.isEnabled, false, `alice was expecting remoteTrack to be 'disabled' in ${roomSid}`);
+  });
+
   [true, false].forEach(trackEnabled => {
-    it(`media track replacement preserves isEnabled state: ${trackEnabled}`, async () => {
+    it(`media track replacement preserves isEnabled state: ${trackEnabled} (@unstable: JSDK-2852)`, async () => {
       // Bob publishes video track
       const bobVideoTrackA = await createLocalVideoTrack(Object.assign({ name: 'trackA' }, smallVideoConstraints));
       await waitFor(bobLocal.publishTrack(bobVideoTrackA), `Bob to publish video trackA: ${roomSid}`);
-
-      bobVideoTrackA.enable(trackEnabled);
-      assert.equal(bobVideoTrackA.isEnabled, trackEnabled);
 
       // wait for Alice to see Bob's track
       const bobRemote = aliceRoom.participants.get(bobRoom.localParticipant.sid);
       await waitFor(tracksSubscribed(bobRemote, 1), `wait for alice to subscribe to Bob's tracks: ${roomSid}`);
 
       const bobRemoteTrack = [...bobRemote.videoTracks.values()][0];
+
+      if (bobVideoTrackA.isEnabled !== trackEnabled) {
+        bobVideoTrackA.enable(trackEnabled);
+        assert.equal(bobVideoTrackA.isEnabled, trackEnabled);
+
+        // wait for alice to see track change.
+        const expectEvent = trackEnabled ? 'enabled' : 'disabled';
+        const trackEnablePromise = await new Promise(resolve => bobRemoteTrack.once(expectEvent, resolve));
+        waitFor(trackEnablePromise, `waiting for alice to see track ${expectEvent} on Bob's track: ${roomSid}`);
+      }
+
       assert.equal(bobRemoteTrack.track.isEnabled, trackEnabled, `alice was expecting remoteTrack to be ${trackEnabled ? 'enabled' : 'disabled'} in ${roomSid}`);
 
       // bob replaces the track.
